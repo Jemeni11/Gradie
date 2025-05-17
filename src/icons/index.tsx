9ec6a9a696cb08e2ecdcafb65980691942d7a355
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2025-05-17 10:22:35 +0100

    feat: add icons

---
+++ src/icons/index.tsx
@@ -0,0 +1,65 @@
+import { SVGProps } from "react";
+
+export const ImageIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <g
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+    >
+      <rect width={18} height={18} x={3} y={3} rx={2} ry={2}></rect>
+      <circle cx={9} cy={9} r={2}></circle>
+      <path d="m21 15l-3.086-3.086a2 2 0 0 0-2.828 0L6 21"></path>
+    </g>
+  </svg>
+);
+
+export const DocumentIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <g
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+    >
+      <path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z"></path>
+      <path d="M14 2v4a2 2 0 0 0 2 2h4"></path>
+      <circle cx={10} cy={12} r={2}></circle>
+      <path d="m20 17l-1.296-1.296a2.41 2.41 0 0 0-3.408 0L9 22"></path>
+    </g>
+  </svg>
+);
+
+export const DeleteIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M3 6h18m-2 0v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6m3 0V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2m-6 5v6m4-6v6"
+    ></path>
+  </svg>
+);
