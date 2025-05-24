2b8f1bc8efeb337acf40c0d9cd7b480449510ade
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2025-05-24 11:40:25 +0100

    feat: add header

---
+++ src/components/views/header.tsx
@@ -0,0 +1,67 @@
+import { useAtom } from "jotai";
+import { colorFormatAtom } from "@/store";
+import { OptionsIcon } from "@/icons";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ColorFormat } from "@/types";
+import { supportedColorFormats } from "@/constants";
+
+export default function Header() {
+  const [colorFormat, setColorFormat] = useAtom(colorFormatAtom);
+
+  return (
+    <header className="mb-8 flex items-center justify-between py-4">
+      <div className="flex items-center gap-x-2">
+        <img
+          src="/favicon-32x32.png"
+          alt="Gradie's logo"
+          width={32}
+          height={32}
+        />
+        <span className="font-aladin text-2xl">Gradie</span>
+      </div>
+      <div className="flex items-center gap-x-2">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              variant="outline"
+              size="icon"
+              className="cursor-pointer"
+              aria-label="Color Format Selector"
+            >
+              <OptionsIcon />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="mr-4 w-fit">
+            <DropdownMenuLabel className="font-aladin text-2xl">
+              Colour Format
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuRadioGroup
+              value={colorFormat}
+              onValueChange={(value) => setColorFormat(value as ColorFormat)}
+            >
+              {supportedColorFormats.map((format) => (
+                <DropdownMenuRadioItem
+                  key={format}
+                  value={format}
+                  className="cursor-pointer"
+                >
+                  {format}
+                </DropdownMenuRadioItem>
+              ))}
+            </DropdownMenuRadioGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </header>
+  );
+}
