1a17ff23c1384866231f99e8de80cc3f3b0d6be4
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2025-05-24 10:34:51 +0100

    feat: add store index file

---
+++ src/store/index.ts
@@ -0,0 +1,20 @@
+import { colorFormatAtom } from "./colorFormatAtom";
+import { imageAtom } from "./imageAtom";
+import { paletteAtom, dominantColorAtom } from "./paletteAtom";
+import { gradientModeAtom } from "./gradientModeAtom";
+import {
+  customPickStartAtom,
+  customPickStopAtom,
+  customPickEndAtom,
+} from "./customPicksAtom";
+
+export {
+  colorFormatAtom,
+  imageAtom,
+  paletteAtom,
+  dominantColorAtom,
+  gradientModeAtom,
+  customPickStartAtom,
+  customPickStopAtom,
+  customPickEndAtom,
+};
