a71137646ab421afafabf34d5ff7318dcafe26e8
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2025-06-10 11:04:32 +0100

    feat: add export for download and dimension constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,6 +3,10 @@ import gradieModes from "./gradieModes";
 import gradientTypes from "./gradientTypes";
 import supportedColorFormats from "./supportedColorFormats";
 import gradientPositions from "./gradientPositions";
+import supportedDownloadFormats from "./supportedDownloadFormats";
+import dimensionPresets from "./dimensionPresets";
+import downloadAspectRatios from "./downloadAspectRatios";
+import dimensionModes from "./dimensionModes";
 
 export {
   colorInterpolationMethodMapper,
@@ -10,4 +14,8 @@ export {
   gradientTypes,
   supportedColorFormats,
   gradientPositions,
+  supportedDownloadFormats,
+  dimensionPresets,
+  downloadAspectRatios,
+  dimensionModes,
 };
