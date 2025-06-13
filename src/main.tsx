9cff8c9ce85a246963148674f8fdf6aa7026aaf5
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2025-06-13 11:13:03 +0100

    analytics: implement PostHog tracking - Add React PostHog provider - Configure with env vars - Set up API host options

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,20 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { PostHogProvider } from "posthog-js/react";
+import type { PostHogConfig } from "posthog-js";
+
+const options: Partial<PostHogConfig> = {
+  api_host: import.meta.env.VITE_APP_PUBLIC_POSTHOG_HOST,
+};
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_APP_PUBLIC_POSTHOG_KEY ?? ""}
+      options={options}
+    >
+      <App />
+    </PostHogProvider>
   </StrictMode>,
 );
