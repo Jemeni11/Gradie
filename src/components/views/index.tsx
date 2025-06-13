29bd30ddcf3f1bdb65eea2b206fab7c9864f2232
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2025-06-13 10:50:54 +0100

    feat: add wiki component.

--- src/components/views/index.tsx
+++ src/components/views/index.tsx
@@ -3,6 +3,15 @@ import Hero from "./hero";
 import PalettePanel from "./palette-panel";
 import GradientPanel from "./gradient-panel";
 import GradientArea from "./gradient-area";
+import Wiki from "./wiki";
 import Footer from "./footer";
 
-export { Header, Hero, PalettePanel, GradientPanel, GradientArea, Footer };
+export {
+  Header,
+  Hero,
+  PalettePanel,
+  GradientPanel,
+  GradientArea,
+  Wiki,
+  Footer,
+};
---
+++ src/components/views/wiki.tsx
@@ -0,0 +1,15 @@
+export default function Wiki() {
+  return (
+    <section className="mt-16 mb-8">
+      <hr />
+      <h2 className="font-aladin mx-auto my-4 w-fit scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Wiki
+      </h2>
+      <hr />
+      <p className="mx-auto mb-16 max-w-2xs text-center text-lg leading-7 lg:max-w-96 [&:not(:first-child)]:mt-6">
+        Gradie is a complicated app. Or at least more complicated than I
+        imagined. So here's a section to help.
+      </p>
+    </section>
+  );
+}
