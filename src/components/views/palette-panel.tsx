58b085e2cc6900a7b78b57d77cc759dae8badc8a
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2025-06-14 15:03:59 +0100

    style: adjust palette panel flex behavior - Add flex-1

--- src/components/views/palette-panel.tsx
+++ src/components/views/palette-panel.tsx
@@ -2,7 +2,7 @@ import ImageUploadInput from "@/components/image-upload-input";
 
 export default function PalettePanel() {
   return (
-    <div className="size-full">
+    <div className="size-full flex-1">
       <ImageUploadInput className="aspect-video w-full" />
     </div>
   );
