9496034d9f2c2ff318af0487133842d6d18c8133
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2025-07-15 22:22:31 +0100

    refactor(views): remove PalettePanel export

--- src/components/views/index.tsx
+++ src/components/views/index.tsx
@@ -1,17 +1,8 @@
 import Header from "./header";
 import Hero from "./hero";
-import PalettePanel from "./palette-panel";
 import GradientPanel from "./gradient-panel";
 import GradientArea from "./gradient-area";
 import Wiki from "./wiki";
 import Footer from "./footer";
 
-export {
-  Header,
-  Hero,
-  PalettePanel,
-  GradientPanel,
-  GradientArea,
-  Wiki,
-  Footer,
-};
+export { Header, Hero, GradientPanel, GradientArea, Wiki, Footer };
