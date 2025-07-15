24d7dab5c51c8acdb106dbbc02a98742053602ac
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2025-07-15 22:23:42 +0100

    style(loader): remove unnecessary flex-1 from loader container

--- src/components/views/loader.tsx
+++ src/components/views/loader.tsx
@@ -2,7 +2,7 @@ import { LoaderIcon } from "@/icons";
 
 export default function Loader() {
   return (
-    <div className="flex flex-1 flex-col items-center justify-center gap-6 py-8">
+    <div className="flex flex-col items-center justify-center gap-6 py-8">
       <div className="relative">
         <LoaderIcon />
       </div>
