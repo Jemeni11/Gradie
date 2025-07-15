f7334a2c0635072a14e8b6377f78c08e088acf4e
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2025-07-15 22:20:43 +0100

    deleted file

--- src/components/views/palette-panel.tsx
+++
@@ -1,9 +0,0 @@
-import ImageUploadInput from "@/components/image-upload-input";
-
-export default function PalettePanel() {
-  return (
-    <div className="size-full flex-1">
-      <ImageUploadInput className="aspect-video w-full" />
-    </div>
-  );
-}
