09f139277433e378357df7f21543f0aad9fa8343
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2025-07-19 16:40:01 +0100

    feat: add default gradients for new home page

---
+++ src/constants/defaultGradient.ts
@@ -0,0 +1,15 @@
+const defaultGradients = [
+  `linear-gradient(
+    180deg,
+    lch(93.75% 1.56 96.9deg) 0%,
+    lch(61.64% 57.44 1.54deg) 100%
+  )`,
+  "linear-gradient(90deg in oklch, oklch(59.327% 0.1897 254.84), oklch(92.065% 0.00256 228.79))",
+  "linear-gradient(90deg, #cd7c69, #060505)",
+  "linear-gradient(90deg, #141414, #d4d4d4)",
+  "radial-gradient(circle at right top in hwb, hwb(356.1 34.118% 17.647%), hwb(260 1.5686% 97.255%))",
+  "radial-gradient(circle at center in hwb, hwb(36 75.686% 20.392%), hwb(18.75 7.8431% 73.333%))",
+  "linear-gradient(90deg in oklch, oklch(21.43% 0.03516 283.03), oklch(43.151% 0.18211 275.26), oklch(81.435% 0.00883 349.3), oklch(50.208% 0.0137 334.33), oklch(70.969% 0.09311 255.44))",
+];
+
+export default defaultGradients;
