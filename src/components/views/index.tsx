109ce4f5776d276a5750a01cab7e51ec76c9eea9
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2025-07-19 16:10:59 +0100

    refactor: replace Wiki with FAQ and export AnalyticsDialogContent

--- src/components/views/index.tsx
+++ src/components/views/index.tsx
@@ -2,7 +2,16 @@ import Header from "./header";
 import Hero from "./hero";
 import GradientPanel from "./gradient-panel";
 import GradientArea from "./gradient-area";
-import Wiki from "./wiki";
+import FAQ from "./faq";
 import Footer from "./footer";
+import AnalyticsDialogContent from "./analytics-dialog-content";
 
-export { Header, Hero, GradientPanel, GradientArea, Wiki, Footer };
+export {
+  Header,
+  Hero,
+  GradientPanel,
+  GradientArea,
+  FAQ,
+  Footer,
+  AnalyticsDialogContent,
+};
