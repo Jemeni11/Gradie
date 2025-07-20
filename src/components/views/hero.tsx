4618df44a95e3902d3a6e78e3555b687bbd41573
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2025-07-20 15:49:59 +0100

    style(hero): reduce bottom margin on hero subtitle

--- src/components/views/hero.tsx
+++ src/components/views/hero.tsx
@@ -4,7 +4,7 @@ export default function Hero() {
       <h1 className="font-aladin mx-auto mb-4 w-fit scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
         From Image to Gradient
       </h1>
-      <p className="mx-auto mb-16 max-w-2xs text-center text-lg leading-7 lg:max-w-96 [&:not(:first-child)]:mt-6">
+      <p className="mx-auto mb-2 max-w-2xs text-center text-lg leading-7 lg:max-w-96 [&:not(:first-child)]:mt-6">
         Build and export gradients from image palettes using Gradie!
       </p>
     </main>
