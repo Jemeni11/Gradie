e3c2ab2cbc2812dffee212849e8feacc68adb93e
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2025-07-20 15:55:50 +0100

    feat(app): Adds a default animated gradient preview and upload dialog shown when no image is present. Preview logic is complete, but dialog content is still WIP.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,42 @@
 import { useEffect, useState } from "react";
 import { Toaster } from "@/components/ui/sonner";
-import { Header, Hero, GradientPanel, FAQ, Footer } from "@/components/views";
+import {
+  Header,
+  Hero,
+  GradientPanel,
+  GradientPreview,
+  FAQ,
+  Footer,
+} from "@/components/views";
 import ImageUploadInput from "@/components/image-upload-input";
 import Banner from "@/components/views/banner";
 import { usePostHog } from "posthog-js/react";
+import { useImageUpload } from "@/hooks/useImageUpload";
+import defaultGradients from "@/constants/defaultGradient";
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogClose,
+  DialogFooter,
+  DialogDescription,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
 
 export default function App() {
   const posthog = usePostHog();
   const [showBanner, setShowBanner] = useState(false);
 
-  useEffect(() => {
-    const analyticsConsent = localStorage.getItem("gradie-analytics-consent");
-    if (analyticsConsent === "true") {
-      posthog?.opt_in_capturing();
-    } else if (analyticsConsent === null) {
-      // Show banner only if user hasn’t decided
-      setShowBanner(true);
-    }
-  }, [posthog]);
+  const [index, setIndex] = useState(
+    Math.floor(Math.random() * defaultGradients.length),
+  );
+  const currentGradient = defaultGradients[index];
+
+  const [opacity, setOpacity] = useState(1);
+
+  const { validFile } = useImageUpload();
 
   const handleEnableAnalytics = () => {
     localStorage.setItem("gradie-analytics-consent", "true");
@@ -30,6 +49,32 @@ export default function App() {
     setShowBanner(false);
   };
 
+  useEffect(() => {
+    const analyticsConsent = localStorage.getItem("gradie-analytics-consent");
+    if (analyticsConsent === "true") {
+      posthog?.opt_in_capturing();
+    } else if (analyticsConsent === null) {
+      // Show banner only if user hasn’t decided
+      setShowBanner(true);
+    }
+  }, [posthog]);
+
+  const fadeDuration = 1000;
+  const displayDuration = 5000;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setOpacity(0); // start fade out
+
+      setTimeout(() => {
+        setIndex((i) => (i + 1) % defaultGradients.length);
+        setOpacity(1); // fade in new gradient
+      }, fadeDuration);
+    }, displayDuration);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
       <Toaster richColors closeButton />
@@ -39,10 +84,66 @@ export default function App() {
       <div className="mx-auto w-full max-w-7xl px-4 md:px-8 lg:h-screen">
         <Header />
         <Hero />
-        <div className="grid grid-cols-1 gap-4 min-[900px]:grid-cols-2">
-          <ImageUploadInput className="aspect-video w-full" />
-          <GradientPanel />
-        </div>
+        {validFile ? (
+          <div className="grid grid-cols-1 gap-4 min-[900px]:grid-cols-2">
+            <ImageUploadInput className="aspect-video w-full" />
+            <GradientPanel />
+          </div>
+        ) : (
+          <div>
+            <div className="my-8 inline-flex w-full justify-center">
+              <Dialog>
+                <DialogTrigger asChild>
+                  <Button
+                    variant="outline"
+                    className="cursor-pointer font-medium"
+                    type="button"
+                  >
+                    Upload an image
+                  </Button>
+                </DialogTrigger>
+                <DialogContent className="h-full max-h-[80%] max-w-xl overflow-y-scroll md:max-h-auto md:min-h-[95%] md:overflow-y-auto">
+                  <DialogHeader>
+                    <DialogTitle>Upload an image</DialogTitle>
+                    <DialogDescription>TBD</DialogDescription>
+                  </DialogHeader>
+                  <div className="flex w-full flex-col gap-4 text-sm leading-relaxed">
+                    WIP. BRB.
+                  </div>
+                  <DialogFooter className="mt-4">
+                    <Button
+                      className="inline-flex cursor-pointer items-center gap-2"
+                      type="button"
+                    >
+                      <span>Create a vibe</span>
+                      <span className="animate-bounce">✨</span>
+                    </Button>
+                    <DialogClose asChild>
+                      <Button
+                        variant="destructive"
+                        className="cursor-pointer"
+                        type="button"
+                      >
+                        Close
+                      </Button>
+                    </DialogClose>
+                  </DialogFooter>
+                </DialogContent>
+              </Dialog>
+            </div>
+            <div className="border-gradie-1 rounded-lg border border-solid p-2">
+              <GradientPreview
+                gradient={currentGradient}
+                className="inset-0 transition-opacity duration-1000"
+                styles={{
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                  opacity,
+                }}
+              />
+            </div>
+          </div>
+        )}
         <FAQ />
         <div className="mt-8">
           TODO
