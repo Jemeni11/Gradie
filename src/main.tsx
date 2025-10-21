3220b3eff7c2bf7a71fb6ed4d2f7b296c93201ad
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2025-10-21 15:09:05 +0100

    refactor: simplify PostHogProvider initialization by using posthog client directly

--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,18 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { PostHogProvider } from "posthog-js/react";
-import type { PostHogConfig } from "posthog-js";
+import posthog from "posthog-js";
 
-const options: Partial<PostHogConfig> = {
+posthog.init(import.meta.env.VITE_APP_PUBLIC_POSTHOG_KEY ?? "", {
   api_host: import.meta.env.VITE_APP_PUBLIC_POSTHOG_HOST,
-};
+  defaults: "2025-05-24",
+  persistence: "memory",
+  opt_out_persistence_by_default: true,
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <PostHogProvider
-      apiKey={import.meta.env.VITE_APP_PUBLIC_POSTHOG_KEY ?? ""}
-      options={options}
-    >
+    <PostHogProvider client={posthog}>
       <App />
     </PostHogProvider>
   </StrictMode>,
